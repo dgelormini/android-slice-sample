084f1542c28819ffef9941f06dca7328c48aa431
dgelormini <derek.gelormini@ww.com>
2020-07-23 14:17:32 -0400

    Add comment, log

--- app/src/main/java/com/dgelormini/slicesamples/SliceInitializer.kt
+++ app/src/main/java/com/dgelormini/slicesamples/SliceInitializer.kt
@@ -11,7 +11,10 @@ class SliceInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         Log.i(TAG, "Granting URI permissions")
 
+        // TODO: This hangs and/or crashes on Android 9!
         SliceUtils.grantAssistantPermissions(context)
+
+        Log.i(TAG, "Granted URI permissions successfully")
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
